36bee7da1f287884b9e07b1aa1a5a9b33e357455
vengelst <ve@vivahome.de>
2026-04-30 17:08:04 +0200

    error

--- abgleich/mail.config.ps1
+++ abgleich/mail.config.ps1
@@ -2,7 +2,7 @@
     remoteName = 'origin'
     branch = 'main'
     repoUrl = 'https://github.com/vengelst/mailpilot.git'
-    serverHost = 'mailpilot.vivahome.de'
+    serverHost = 'vivahome.de'
     serverUser = 'root'
     serverPath = '/opt/mailpilot'
     nginxConfigLocalPath = 'deploy/nginx/mailpilot.vivahome.de.conf'
